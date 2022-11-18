62db6f2d5688c486705cebfaa517490dcca711aa
unknown <stolyarova.199535@gmail.com>
2022-11-18 19:23:53 +0300

    шапка

--- source/js/main.js
+++ source/js/main.js
@@ -13,6 +13,40 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
+  const breakpoint = window.matchMedia('(min-width:767px)');
+
+  // Header
+
+  const headerButton = document.querySelector('.header__button');
+  const headerList = document.querySelector('.header__list');
+  const headerButtonText = document.querySelector('.header__text');
+  const headerLogo = document.querySelector('.header__logo');
+
+  const breakpointChecker = () => {
+    if (breakpoint.matches) {
+      headerList.style.display = 'flex';
+    } else {
+      headerList.style.display = 'none';
+    }
+  };
+
+  breakpoint.addEventListener('change', breakpointChecker);
+  breakpointChecker();
+
+  headerButton.addEventListener('click', () => {
+    if (headerButton.getAttribute('data-state') === 'active') {
+      headerLogo.style.display = 'block';
+      headerList.style.display = 'none';
+      headerButton.setAttribute('data-state', '');
+      headerButtonText.textContent = 'Открыть меню';
+    } else {
+      headerLogo.style.display = 'none';
+      headerList.style.display = 'flex';
+      headerButton.setAttribute('data-state', 'active');
+      headerButtonText.textContent = 'Закрыть меню';
+    }
+  });
+
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
