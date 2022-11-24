e758b51f8de233d7f3e23248abaa66aa460c7a86
unknown <stolyarova.199535@gmail.com>
2022-11-24 19:35:23 +0300

    все

--- source/js/main.js
+++ source/js/main.js
@@ -21,6 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const headerList = document.querySelector('.header__list');
   const headerButtonText = document.querySelector('.header__text');
   const headerLogo = document.querySelector('.header__logo');
+  const headerLinks = document.querySelectorAll('.header__link');
 
   const breakpointChecker = () => {
     if (breakpoint.matches) {
@@ -44,6 +45,88 @@ window.addEventListener('DOMContentLoaded', () => {
       headerList.style.display = 'flex';
       headerButton.setAttribute('data-state', 'active');
       headerButtonText.textContent = 'Закрыть меню';
+    }
+  });
+
+  headerLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const id = link.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  });
+
+  // Form
+  const telephones = document.querySelectorAll('input[type="tel"]');
+
+  const prefixNumber = (str) => {
+    if (str === '7') {
+      return '7 (';
+    }
+    if (str === '8') {
+      return '7 (';
+    }
+    if (str === '9') {
+      return '7 (';
+    }
+    return '7 (';
+  };
+
+  // ---------------
+  for (let i = 0; i < telephones.length; i++) {
+    telephones[i].addEventListener('input', () => {
+      const value = telephones[i].value.replace(/\D+/g, '');
+      const numberLength = 11;
+
+      let result;
+      if (telephones[i].value.includes('+8') || telephones[i].value[0] === '8') {
+        result = '';
+      } else {
+        result = '+';
+      }
+
+      //
+      for (let j = 0; j < value.length && j < numberLength; j++) {
+        switch (j) {
+          case 0:
+            result += prefixNumber(value[i]);
+            continue;
+          case 4:
+            result += ') ';
+            break;
+          case 7:
+            result += '-';
+            break;
+          case 9:
+            result += '-';
+            break;
+          default:
+            break;
+        }
+        result += value[j];
+      }
+      //
+      telephones[i].value = result;
+    });
+  }
+
+  const form = document.querySelector('.form');
+  const formCheckbox = form.querySelector('#checkbox');
+  const formLabel = form.querySelector('#label-checkbox');
+
+  form.addEventListener('submit', (evt) => {
+    if (formCheckbox.checked === false) {
+      evt.preventDefault();
+      formLabel.style.boxShadow = '0 0 0 3px red';
+    }
+
+    if (telephones[0].value.length < 18) {
+      evt.preventDefault();
+      telephones[0].style.boxShadow = '0 0 0 3px red';
     }
   });
 
